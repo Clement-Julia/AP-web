ec35e0b33ec9073db9eed43aaa31716774660962
Clement-Julia <81628168+Clement-Julia@users.noreply.github.com>
2022-03-31 14:40:20 +0200

    fix mail template

--- mail/template_reset.php
+++ mail/template_reset.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <body style="background-color: rgb(230 236 239)">
+    <body>
+        <div style="background-color: rgb(230,236,239) !important">
+            <div style="background-color: white !important; width:50% !important;margin: 0 auto !important">
 
-        <div style="background-color: white; width:50%;margin: 0 auto;">
-            <div style="text-align:center">
-                <img id="logo" src="../assets/src/img/locallacol.png" alt="Logo du site locallacol" style="width:30%;margin-top:20px"><br>
-                <p style="color: rgb(212 33 33);text-transform: uppercase;">Demande de réinitialisation de mot de passe</p>
-            </div>
-            
-            <div style="padding:5%; color:#999999">
-                <?php echo "test : " . print_r($_POST) ?>
-                <b>Bonjour {name} {fistname},</b>
-                <p>Le code unique à saisir pour confirmer la réinitialisation de votre mot de passe est le {code} . <br>
- 
-                Ce code est valable pour une durée de 10 minutes. Au-delà, vous devrez recommencer la procédure.</p>
+                <div style="text-align:center !important">
+                    <img id="logo" src="https://loocalacool.ipssi-sio.fr/assets/src/img/locallacol.png" alt="Logo du site loocalacool" style="width:30% !important;margin-top:20px !important"><br>
+                    <p style="color: rgb(212,33,33) !important;text-transform: uppercase !important">Demande de réinitialisation de mot de passe</p>
+                </div>
                 
-                <p>Vous n'êtes pas à l'origine de cette opération ? Changez votre mot-de-passe par mesure de sécurité.</p>
-                
-                
-                A bientôt sur votre Espace Client,<br>
-                
-                L'équipe loocalacool,<br>
-                <a href="{lien}"><img id="logo" src="../assets/src/img/locallacol.png" alt="Logo du site locallacol" style="width:15%;margin-top:10px"><br></a>
-            </div>
+                <div style="padding:5% !important; color:#999999 !important">
+                    <b>Bonjour {name} {fistname},</b>
+                    <p>Le code unique à saisir pour confirmer la réinitialisation de votre mot de passe est le {code} .</p>
+    
+                    <p>Ce code est valable pour une durée de 10 minutes. Au-delà, vous devrez recommencer la procédure.
+                    
+                    <p>Vous n'êtes pas à l'origine de cette opération ? Changez votre mot-de-passe par mesure de sécurité.</p>
+                    
+                    
+                    <p>A bientôt sur votre Espace Client,</p>
+                    
+                    L'équipe loocalacool,<br>
+                    <a href="{lien}"><img id="logo" src="https://loocalacool.ipssi-sio.fr/assets/src/img/locallacol.png" alt="Logo du site loocalacool" style="width:15% !important;margin-top:15px !important"><br></a>
+                </div>
 
+            </div>
         </div>
-
     </body>
 </html>
