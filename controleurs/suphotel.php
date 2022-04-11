ec097f79ec04df6d85db35486c011a2d06b07574
Clement-Julia <81628168+Clement-Julia@users.noreply.github.com>
2022-04-11 22:53:52 +0200

    maj

--- controleurs/suphotel.php
+++ controleurs/suphotel.php
@@ -13,9 +13,9 @@ if($_SESSION["idRole"] == 2){
     try{
         $hotel->supHotel($_SESSION["supHotel"], $hotel->getUuid());
         $options->supOptions($_SESSION["supHotel"]);
-        header("location:../admin/modifHotel?success.php");
+        header("location:../admin/modifHotel.php?success");
     }catch(exception $e){
-        header("location:../admin/modifHotel?crash.php");
+        header("location:../admin/modifHotel.php?crash");
     }
 }else{
     header("location:../vues/");
